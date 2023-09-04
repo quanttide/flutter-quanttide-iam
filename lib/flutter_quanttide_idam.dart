247c1bc7bb8e84fca10b94d4684ee68ba4761838
Guo Zhang <zhangguo@quanttide.com>
2023-09-04 16:51:42 +0800

    init: create project

---
+++ lib/flutter_quanttide_idam.dart
@@ -0,0 +1,4 @@
+// 页面
+export 'src/screens/login.dart';
+// 数据模型
+export 'src/models/user.dart';
---
+++ lib/src/models/user.dart
@@ -0,0 +1,20 @@
+/// 用户模型
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../../flutter_quanttide.dart';
+
+part 'user.freezed.dart';
+part 'user.g.dart';
+
+
+/// 用户数据模型
+@Freezed()
+class UserModel with _$UserModel {
+  const factory UserModel({
+    required String id,
+  }) = _UserModel;
+
+  factory UserModel.fromJson(Map<String, dynamic> json)
+  => _$UserModelFromJson(convertKeysToCamel(json));
+}
